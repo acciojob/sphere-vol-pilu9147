7f99e9625e306f2bed07f320252ab6ece35c9737
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-05-22 15:33:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function volume_sphere() {
     //Write your code here
-  
+     var radius = document.getElementById('radius').value;
+    var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    document.getElementById('volume').value = volume;
+
+    // Prevent form submission
+    return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
